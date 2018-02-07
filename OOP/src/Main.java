156b8999fd843d62f72efb7fb403a10f7dac678c
Ihor-Moroz <ihor.moroz.ukr@gmail.com>
2018-02-07 18:14:24 +0200

    Створення об'єектів

--- OOP/src/Main.java
+++ OOP/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
+    static Cat cat= new Cat();
+    static People people=new People();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        cat.name="John";
+        cat.age=3;
+        cat.poroda="Brodyaga";
+
+        people.name="Ihor";
+        people.surname="Surname";
+        people.age=20;
+        people.cat=cat;
+
+        System.out.println(people);
+        cat.say();
+        people.cat.say();
     }
 }
